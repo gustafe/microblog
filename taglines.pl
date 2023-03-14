4ab7cc11ae235c37ac4e5602c3a7ea0d39659416
Gustaf Erikson <gerikson@gmail.com>
2023-03-14 08:12:49 +0100

    maybe new feature to rotate the taglines on top of the blog

---
+++ taglines.pl
@@ -0,0 +1,45 @@
+#! /usr/bin/env perl
+
+# significant parts of this code is adapted from John Bokma's
+# Tumblelog project, available at
+# https://github.com/john-bokma/tumblelog
+
+use Modern::Perl '2015';
+use CommonMark qw(:opt :node :event);
+use Data::Dump qw/dump/;
+use Digest::MD5 qw/md5_hex/;
+use Encode;
+use FindBin qw/$Bin/;
+use JSON::XS;
+use List::Util qw/min max/;
+use Path::Tiny;
+use Template;
+use Text::Unidecode;
+use Time::HiRes qw/gettimeofday tv_interval/;
+use Time::Piece;
+use Unicode::Normalize;
+use XML::Atom::SimpleFeed;
+
+use utf8;
+use open qw/ :std :encoding(utf8) /;
+binmode( STDOUT, ":encoding(UTF-8)" );
+
+my $debug = 0;
+
+my %config = ( taglines => './Content/taglines.fortune');
+
+my $taglines = read_entries( $config{taglines} );
+
+my $entry = $taglines->[rand @$taglines];
+
+print $entry;
+sub read_entries {
+
+    my $filename = shift;
+    my $entries
+        = [ grep { length $_ } split /^%\n/m, path($filename)->slurp_utf8() ];
+
+    @$entries or die 'No entries found';
+
+    return $entries;
+}
